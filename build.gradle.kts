bc2de754bca58aab7b421ca6a4c2f32aa1d0246e
Saran <saranshajibabu@gmail.com>
2021-06-08 16:17:53 +0800

    deps: add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.21")
+        classpath("com.squareup.sqldelight:gradle-plugin:1.4.3")
         classpath("com.android.tools.build:gradle:4.0.1")
     }
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,20 +2,50 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    kotlin("native.cocoapods")
     id("com.android.library")
+    id("com.squareup.sqldelight")
+}
+
+repositories {
+    gradlePluginPortal()
+    google()
+    jcenter()
+    mavenCentral()
+    maven(url = "https://kotlin.bintray.com/kotlinx/")
 }
 
 kotlin {
+    val coroutinesVersion = "1.3.9-native-mt"
+    val serializationVersion = "1.0.0-RC"
+    val ktorVersion = "1.4.0"
+    val sqlDelightVersion = "1.4.3"
+
     android()
-    ios {
-        binaries {
-            framework {
-                baseName = "shared"
+
+    /* If you don't have Xcode installed comment this code block*/
+//    val onPhone = System.getenv("SDK_NAME")?.startsWith("iphoneos") ?: false
+//    if (onPhone) {
+//        iosArm64("ios")
+//    } else {
+//        iosX64("ios")
+//    }
+
+
+
+    sourceSets {
+        val commonMain by getting{
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+                implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
             }
         }
-    }
-    sourceSets {
-        val commonMain by getting
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -25,6 +55,8 @@ kotlin {
         val androidMain by getting {
             dependencies {
                 implementation("com.google.android.material:material:1.2.1")
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+                implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
             }
         }
         val androidTest by getting {
@@ -33,8 +65,12 @@ kotlin {
                 implementation("junit:junit:4.13")
             }
         }
-        val iosMain by getting
-        val iosTest by getting
+//        val iosMain by getting
+//        val iosTest by getting
+//        dependencies {
+//            implementation("io.ktor:ktor-client-ios:$ktorVersion")
+//            implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
+//        }
     }
 }
 
@@ -47,17 +83,23 @@ android {
     }
 }
 
-val packForXcode by tasks.creating(Sync::class) {
-    group = "build"
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
-    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
-    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
-    inputs.property("mode", mode)
-    dependsOn(framework.linkTask)
-    val targetDir = File(buildDir, "xcode-frameworks")
-    from({ framework.outputDirectory })
-    into(targetDir)
-}
+//val packForXcode by tasks.creating(Sync::class) {
+//    group = "build"
+//    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+//    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
+//    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
+//    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
+//    inputs.property("mode", mode)
+//    dependsOn(framework.linkTask)
+//    val targetDir = File(buildDir, "xcode-frameworks")
+//    from({ framework.outputDirectory })
+//    into(targetDir)
+//}
+//
+//tasks.getByName("build").dependsOn(packForXcode)
 
-tasks.getByName("build").dependsOn(packForXcode)
+sqldelight {
+    database("AppDatabase") {
+        packageName = "com.alpha.kmmnetworkdbsample.shared.cache"
+    }
+}
